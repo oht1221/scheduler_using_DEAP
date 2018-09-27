568797a0af835db353d308f020dfd07962b756ae
oht1221 <oht1221@gmail.com>
2018-09-27 22:21:02 +0900

    asd

--- read_from_excel.py
+++ read_from_excel.py
@@ -3,26 +3,126 @@ import datetime
 import sys
 from tkinter import filedialog
 from tkinter import *
+from tkinter import messagebox
 import time
 from evaluation import Machine
 from job import *
 from accessDB import *
-from preprocessing import search_cycle_time
+from preprocessing import search_cycle_time, read_CNCs
 from displays_results import print_out_unit
+import copy
 
-root = Tk()
+VALVE_PRE_CNCs = [1, 2, 3, 32, 33, 34, 37, 38, 44]
+LOK_FORGING_CNCs = [10, 15]
+LOK_HEX_CNCs = [8, 9, 11, 12, 13]
 indiv = []
 work_numbers = []
+CNCs = []
+read_CNCs('./장비정보.xlsx', CNCs)
 
 machines = {}
 new_job = None
 
-def inputFile():
+CNCs_2jaw = list(filter(lambda x: x.getShape() == 0, CNCs))
+CNCs_3jaw = list(filter(lambda x: x.getShape() == 1, CNCs))
+CNCs_round = list(filter(lambda x: (x.getShape() == 1 and not (x.getNote() == "코렛")), CNCs))
+CNCs_square = list(filter(lambda x: x.getShape() == 0, CNCs))
+CNCs_valve_pre = list(filter(lambda x: x.getNumber() in VALVE_PRE_CNCs, CNCs))
+CNCs_LOK_size_forging = list(filter(lambda x: x.getNumber() in LOK_FORGING_CNCs, CNCs))
+CNCs_LOK_size_hex = list(filter(lambda x: x.getNumber() in LOK_HEX_CNCs, CNCs))
+
+root = Tk()
+
+
+def score(assignments):
+    TOTAL_DELAYED_JOBS_COUNT = 0
+    TOTAL_DELAYED_TIME = 0
+
+    for job in assignments:
+        components = job.getComponents()
+        last_component = components[-1]  # 마지막 공정이 끝난 시간
+        job_end_time = last_component.getEndDateTime()
+        diff = job.getDue() - (job_end_time + 60 * 60 * 24 * 5)  # 5일간 다른 공정
+
+        if diff < 0:
+            job.delayed()
+            TOTAL_DELAYED_JOBS_COUNT += 1
+            TOTAL_DELAYED_TIME += (-1) * diff
+            for comp in components:
+                comp.delayed()
+    return TOTAL_DELAYED_TIME
+
+def insert(CNC_list, insertion):
+    selected_machines = [machines[c.cnc_number()] for c in CNC_list]
+
+    for machine in selected_machines:
+        assignments = machine.getAssignments()
+        new_assignments = copy.deepcopy(assignments)
+
+        i = 0
+        while 1:
+            for j, comp in insertion.items():
+                new_assignments.insert(i+j, comp)
+            i += 2
+
+            if i >= len(assignments):
+                break
+        if insertion[-1].
+
+
+def makeInsertion(job):
+
+    insertion = list()
+    for comp in job.getComponents():
+        insertion.append(comp)
+        setting_time = Component(cycleTime=60 * 45, quantity=1, processCd=None, ifsetting=True)
+        insertion.append(setting_time)
+
+    return insertion
+
+def insertAndCheck():
+
+    TOTAL_DELAYED_JOBS_COUNT = 0
+    TOTAL_DELAYED_TIME = 0
+    insertion = makeInsertion(new_job)
+
+    new_assignments = None
+
+    if new_job.getLokFittingSize() == 1:
+        if new_job.getType() == 0:
+            new_assignments = insert(CNCs_LOK_size_forging, insertion)
+
+        elif new_job.getType() == 1:
+            new_assignments = insert(CNCs_LOK_size_hex, insertion)
+
+    elif new_job.getType() == 0:
+        new_assignments = insert(CNCs_2jaw, insertion)
+
+    elif new_job.getType() == 1:
+        new_assignments = insert(CNCs_3jaw, insertion)
+
+    elif new_job.getType() == 2:
+        new_assignments = insert(CNCs_round, insertion)
+
+    elif new_job.getType() == 3:
+        new_assignments = insert(CNCs_square, insertion)
+
+    elif new_job.getType() == 4:
+        new_assignments = insert(CNCs_valve_pre, insertion)
+
+    else:
+        print("job type error!")
+
+
+def readFile():
     root.filename = filedialog.askopenfilename(initialdir = "./schedules",
                                                title = 'choose your schdule',
                                                filetypes = (('excel files', '*.xlsx'),
                                                             ('excel files', '*.xls'))
                                                )
+    read_schedule(root.filename)
+
+    messagebox.showinfo("message", "파일 읽기 완료!")
 
 def inputVariables(new_job):
     workno = input_workno.get()
@@ -38,7 +138,9 @@ def inputVariables(new_job):
     cursor = AccessDB()
     cycle_time = []
     gubun = input_gubun.get()
+
     search_cycle_time(cursor, cycle_time, goodCd)
+
     new_job = Job(workno=workno, goodCd=goodCd, type=gubun, quantity=qty, time=cycle_time,
                               due=due_date_seconds)
 
@@ -53,7 +155,7 @@ def read_schedule(input):
     standard = row[1]
     standard = (lambda x: int(time.time()) if (x == 'now') else time.mktime(
         (int(x[0:4]), int(x[4:6]), int(x[6:8]), 12, 0, 0, 0, 0, 0)))(standard)
-    print(standard)
+
 
     i = 1
     worksheet = workbook.sheet_by_index(i)
@@ -61,7 +163,6 @@ def read_schedule(input):
     while worksheet is not None:
         M = Machine()
         machines[worksheet.name] = M
-        print(worksheet.name)
 
         assignment = M.getAssignments()
         n_row = worksheet.nrows
@@ -70,6 +171,7 @@ def read_schedule(input):
 
             row = worksheet.row_values(r + 1)
             workno = row[0]
+            goodCd = row[2]
             start = row[5]
             end = row[6]
             start_date_seconds = time.mktime(
@@ -91,8 +193,6 @@ def read_schedule(input):
                 continue
 
             processCd = int((row[1])[1])
-            goodCd = row[2]
-            print(goodCd)
             gubun = row[3]
             due_date = row[7]
             qty = row[8]
@@ -121,11 +221,14 @@ def read_schedule(input):
         except Exception:
             break
 
-btn1 = Button(root, text = "스케줄 선택", command = inputFile)
+    print("파일 입력 완료!")
+
+btn1 = Button(root, text = "스케줄 선택", command = readFile)
 btn1.grid(row = 1)
+
 btn2 = Button(root, text = "완료", command = inputVariables)
 btn2.bind(inputVariables)
-btn2.grid(row = 8)
+btn2.grid(row = 9)
 
 workno = StringVar()
 due = StringVar()
@@ -145,6 +248,8 @@ lbl_goodNo = Label(root, width = 15, text = "품번(숫자, 문자)")
 lbl_goodCd = Label(root, width = 15, text = "품번 코드(숫자)")
 lbl_qty = Label(root, width = 15, text = "수량")
 lbl_gubun = Label(root, width = 15, text = "타입구분")
+lbl_show_state = Label(root)
+
 
 input_workno.grid(row = 2, column = 2)
 input_due.grid(row = 3, column = 2)
@@ -160,6 +265,8 @@ lbl_goodNo.grid(row = 4, column = 1)
 lbl_goodCd.grid(row = 5, column = 1)
 lbl_qty.grid(row = 6, column = 1)
 lbl_gubun.grid(row = 7, column = 1)
+lbl_show_state.grid(row = 8)
+
 
 root.mainloop()
 
